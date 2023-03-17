793b7a4ffb4ecf673cd176d9b2b60efb9e994bdf
david monir <34280997+davidmonir17@users.noreply.github.com>
2023-03-17 12:53:52 +0200

    add statues Repository

---
+++ Repository/Implmntation/StatuesRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Context;
+using Domain.Entities;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implmntation
+{
+    public class StatuesRepository : RepositoryBase<Statues>, IStatuesRepository
+    {
+        public StatuesRepository(DataBaseContext dataBase) : base(dataBase)
+        {
+        }
+
+        public void AddStatues(Statues statues)
+        {
+            Add(statues);
+        }
+
+        public IEnumerable<Statues> GetAllStatues()
+        {
+            return GetAll().OrderBy(x => x.Id).ToList();
+        }
+
+        public Statues GetStatues(int id)
+        {
+            return Find(x => x.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ Repository/Interface/IStatuesRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    public interface IStatuesRepository
+    {
+        void AddStatues(Statues statues);
+
+        Statues GetStatues(int id);
+
+        IEnumerable<Statues> GetAllStatues();
+    }
+}
